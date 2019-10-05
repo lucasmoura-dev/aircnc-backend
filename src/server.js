8c07ef4258c285f13f04621bc3cb3b561912de67
Lucas Moura <lucasmoura.developer@hotmail.com>
2019-10-04 21:54:19 -0300

    featuer: added mongodb connection string

--- src/server.js
+++ src/server.js
@@ -1,7 +1,14 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const routes = require("./routes");
+require("dotenv").config();
 
 const app = express();
+
+mongoose.connect(process.env.DB_CONNECTION_STRING, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 app.use(express.json());
 
