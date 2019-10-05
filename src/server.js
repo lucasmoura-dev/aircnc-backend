4793b74bb49b946ea0a55e0627db25ebf0003367
Lucas Moura <lucasmoura.developer@hotmail.com>
2019-10-05 01:32:08 -0300

    chore: create thumbnail url as virtua field

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 const routes = require('./routes');
 require('dotenv').config();
 
@@ -15,5 +16,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.listen(3333);
