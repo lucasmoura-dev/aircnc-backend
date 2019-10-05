0d657875a4aad0562bec24c7e5174fc39b921b3b
Lucas Moura <lucasmoura.developer@hotmail.com>
2019-10-04 22:58:11 -0300

    chore: added index route to list Spots

--- src/routes.js
+++ src/routes.js
@@ -9,6 +9,7 @@ const upload = multer(uploadConfig);
 
 routes.post("/sessions", SessionController.store);
 
+routes.get("/spots", upload.single("thumbnail"), SpotController.index);
 routes.post("/spots", upload.single("thumbnail"), SpotController.store);
 
 module.exports = routes;
