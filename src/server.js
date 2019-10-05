14eeae351277bc34419c314dcb2c063a0934cfbb
Lucas Moura <lucasmoura.developer@hotmail.com>
2019-10-05 00:47:20 -0300

    fix: cors errors

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const routes = require("./routes");
-require("dotenv").config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const routes = require('./routes');
+require('dotenv').config();
 
 const app = express();
 
@@ -10,6 +11,7 @@ mongoose.connect(process.env.DB_CONNECTION_STRING, {
   useUnifiedTopology: true
 });
 
+app.use(cors());
 app.use(express.json());
 
 app.use(routes);
